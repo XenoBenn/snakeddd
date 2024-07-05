805987853012d074ad65d00b402cfc4dd6f2f964
XenoBenn <benjamin.komar@gmail.com>
2024-07-05 07:59:09 +0200

    Fast commit

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -11,26 +11,55 @@
 #include <iostream>
 
 
+Color green = {173, 204, 96, 255};
+Color dark_green = {43, 51, 24, 255};
+
+const int cell_size = 30;
+const int cell_count = 5;
+
+const int window_height = cell_size * cell_size;
+const int window_width = cell_size * cell_size;
+	
+class Food {
+
+	public:
+		Vector3 position;
+
+		Food() {
+			position = GenerateRandomPos();
+			std::cout << position.x;
+		}
+
+		void Draw() {
+			DrawCube(position, 2.0f, 2.0f, 2.0f, dark_green);
+			DrawCubeWires(position, 2.0f, 2.0f, 2.0f, RED);
+		}
+
+		Vector3 GenerateRandomPos() {
+			float x = GetRandomValue( 0, cell_count - 1 );
+			float y = GetRandomValue( 0, cell_count - 1 );
+			float z = GetRandomValue( 0, cell_count - 1 );
+			return Vector3{x, y, z};
+		}
+};
 
 int main ()
 {
-	const int window_height = 1280;
-	const int window_width = 800;
 
 	// set up the window
-	InitWindow(window_height, window_width, "Scholaris");
+	InitWindow(window_height, window_width, "SnakeDDD");
 
-	Vector3 camera_position = { 0.0f, 10.0f, 10.0f };
+	Vector3 camera_position = { 0.0f, 20.0f, 14.0f };
 
 	// Define the camera to look into the world
 	Camera3D camera;
 	camera.position = camera_position;
 	camera.target = { 0.0f, 0.0f, 0.0f };
 	camera.up = { 0.0f, 1.0f, 0.0f };
-	camera.fovy = 45.0f;
+	camera.fovy = 55.0f;
 	camera.projection = CAMERA_PERSPECTIVE;
 
-	Vector3 cube_position = {0.0f, 0.0f, 0.0f};
+	Food food = Food();
 	
 	// game loop
 	while (!WindowShouldClose())
@@ -40,13 +69,17 @@ int main ()
 		ClearBackground(RAYWHITE);
 
 		BeginMode3D(camera);
-			DrawCube(cube_position, 2.0f, 2.0, 2.0f, GRAY);
-			DrawCubeWires(cube_position, 2.0f, 2.0, 2.0f, RED);
+			food.Draw();
 
-			DrawGrid(10.0f, 10.0f);
+			for (int i = -1 * cell_count; i < cell_count; i+=2) {
+				for (int j = -1 * cell_count; j < cell_count; j+=2) {
+					for (int k = -1 * cell_count; k < cell_count; k+=2) {
+						DrawCubeWires(Vector3{(float)i, (float)j, (float)k}, 2.0f, 2.0f, 2.0f, GRAY);
+					}
+				}
+			}
+			// DrawGrid(10, 1.0f);
 		EndMode3D();
-
-		DrawText("Hello Raylib", 200,200,20,WHITE);
 
 		EndDrawing();
 	}
